4b9e05236b5b7d9ca86e150703bbb510c2ba5211
YuvalRam <126417248+YuvalRam@users.noreply.github.com>
2024-02-25 09:43:43 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package yuval;
+
+public class L25_2 {
+
+	public static void main(String[] args) {
+		Queue<Integer> q = new Queue<Integer>();
+		q.insert(2);
+		q.insert(4);
+		q.insert(4);
+		q.insert(4);
+		q.insert(8);
+		q.insert(5);
+		q.insert(8);
+		System.out.println(q1(q,4));
+	}
+	//page 151
+	public static boolean q1(Queue<Integer> q, int x) {
+		q.insert(null);
+		boolean b = false;
+		while(q.head()!=null) {
+			if(q.head()==x) {
+				q.insert(q.remove());
+				if(q.head()!=null)
+					if(q.head()==x)
+						b = true;
+			}
+			if(q.head()!=null)
+				q.insert(q.remove());
+		}
+		q.remove();
+		return b;
+	}
+
 }
